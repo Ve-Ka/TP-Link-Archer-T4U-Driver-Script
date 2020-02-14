dfbf03558186c84bdb0b26878cd90e62ab4bce46
Ve Ka <57703430+Ve-Ka@users.noreply.github.com>
2020-02-15 04:35:16 +0800

    Changes to method of installing rtl8822bu driver

--- TP-Link-T4U-Driver-Script.sh
+++ TP-Link-T4U-Driver-Script.sh
@@ -7,6 +7,7 @@ function update_pi ()
 {
   sudo apt-get update
   sudo apt-get upgrade
+  #sudo apt full-upgrade
 }
 
 
@@ -16,25 +17,20 @@ function install_lshw ()
 }
 
 
-function update_dkms_with_driver ()
-{
-  sudo apt install git dkms build-essential
-  sudo dkms add ./rtl8822bu
-  sudo dkms install 8822bu/1.1 
-}
-
-
 function install_rtl8822bu ()
 {
-  cd /home/pi/Downloads/
+  sudo apt install git dkms build-essential 
+  cd /home/pi/Downloads/ 
   git clone https://github.com/jeremyb31/rtl8822bu.git
+  sudo dkms add ./rtl8822bu
+  sudo dkms install 8822bu/1.1
   cd /home/pi/Downloads/rtl8822bu
-  sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile
+  sed -i 's/CONFIG_PLATFORM_I386_PC = y/CONFIG_PLATFORM_I386_PC = n/g' Makefile 
   sed -i 's/CONFIG_PLATFORM_ARM_RPI = n/CONFIG_PLATFORM_ARM_RPI = y/g' Makefile
   sudo make
   sudo make install
   cd /home/pi/Downloads
-  sudo rm -rf rtl8822bu
+  #sudo rm -rf rtl8822bu
 }
 
 
@@ -43,8 +39,8 @@ function install_network_manager ()
   sudo apt-get install network-manager network-manager-gnome
   sudo sysremctl stop dhcpcd.service
   sudo systemctl disable dhcpcd.service
-  sudo enable NetworkManager.service
-  sudo start NetworkManager.service
+  sudo systemctl enable NetworkManager.service
+  sudo systemctl start NetworkManager.service
 }
 
 
@@ -53,10 +49,8 @@ while true ; do
   echo Menu
   echo 1. update pi
   echo 2. install lshw
-  echo 3. update dkms with rtl8822bu
-  echo 4. install rtl8822bu 
-  echo 5. install network manager
-  echo 6. execute all above
+  echo 3. install rtl8822bu
+  echo 4. install network manager
   echo e. exit
 
   read -p "Choice: " choice
@@ -67,15 +61,12 @@ while true ; do
 
   elif [ "$choice" == "2" ]; then
     install_lshw
-
+  
   elif [ "$choice" == "3" ]; then
-    update_dkms_with_driver
-
-  elif [ "$choice" == "4" ]; then
     install_rtl8822bu
 
-  elif [ "$choice" == "5" ]; then
-    install_network_manager 
+  elif [ "$choice" == "4" ]; then
+    install_network_manager  
 
   elif [ "$choice" == "e" ]; then
     echo programme exiting...
